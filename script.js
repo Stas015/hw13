6d36014db7463a4eb0e13a89053860faf657381b
Stas015 <167779562+Stas015@users.noreply.github.com>
2024-06-26 22:13:29 +0200

    script.js

--- script.js
+++ script.js
@@ -37,18 +37,16 @@ const vehicle1 = {
   // Створення масиву vehicles
   const vehicles = [vehicle1, vehicle2, vehicle3, vehicle4, vehicle5];
   
-  // Виведення інформації про всі транспортні засоби
-  console.log("Інформація про всі транспортні засоби:");
-  for (let i = 0; i < vehicles.length; i++) {
-    const vehicle = vehicles[i];
-    console.log(`Модель: ${vehicle.model}, Тип: ${vehicle.type}, Рік випуску: ${vehicle.year}, Робочий стан: ${vehicle.isOperational}`);
-  }
-  
-  // Виведення інформації про транспортні засоби, які є у робочому стані
-  console.log("\nІнформація про транспортні засоби, які є у робочому стані:");
-  for (let i = 0; i < vehicles.length; i++) {
-    const vehicle = vehicles[i];
-    if (vehicle.isOperational) {
-      console.log(`Модель: ${vehicle.model}, Тип: ${vehicle.type}, Рік випуску: ${vehicle.year}`);
+// Виведення інформації про всі транспортні засоби
+console.log("Всі транспортні засоби:");
+for (let i = 0; i < vehicles.length; i++) {
+    console.log(`Модель: ${vehicles[i].model}, Тип: ${vehicles[i].type}, Рік випуску: ${vehicles[i].year}, Робочий стан: ${vehicles[i].isOperational}`);
+}
+
+// Виведення інформації про транспортні засоби, які є у робочому стані
+console.log("\nТранспортні засоби у робочому стані:");
+for (let i = 0; i < vehicles.length; i++) {
+    if (vehicles[i].isOperational) {
+        console.log(`Модель: ${vehicles[i].model}, Тип: ${vehicles[i].type}, Рік випуску: ${vehicles[i].year}, Робочий стан: ${vehicles[i].isOperational}`);
     }
-  }
+}
